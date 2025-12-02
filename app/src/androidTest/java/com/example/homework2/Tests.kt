4bbcfe786d74fa8d2ee7aa609447ad26d0a33101
Mikhail Ulanov <mixaulanoff@yandex.ru>
2025-12-02 16:59:14 +0300

    add some unit tests

---
+++ app/src/androidTest/java/com/example/homework2/Tests.kt
@@ -0,0 +1,54 @@
+package com.example.homework2
+
+import androidx.compose.ui.unit.dp
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.homework2.ui.layout.Paddings
+import com.example.homework2.ui.layout.Size
+import com.example.homework2.viewModel.CatViewModel
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+@RunWith(AndroidJUnit4::class)
+class Tests {
+
+    @Test
+    fun test_catsMessagesIds() {
+        val catMessageIds = listOf(
+            R.string.catMessage_1,
+            R.string.catMessage_2,
+            R.string.catMessage_3,
+            R.string.catMessage_4,
+            R.string.catMessage_5,
+            R.string.catMessage_6,
+            R.string.catMessage_7,
+            R.string.catMessage_8,
+            R.string.catMessage_9,
+            R.string.catMessage_10,
+        )
+
+        val sut = CatViewModel()
+
+        assertEquals(catMessageIds, sut.catMessageIds)
+    }
+
+    @Test
+    fun test_smallPadding() {
+        val sut = Paddings
+        assertEquals(8.dp, sut.small)
+    }
+
+    @Test
+    fun test_mediumPadding() {
+        val sut = Paddings
+        assertEquals(16.dp, sut.medium)
+    }
+
+    @Test
+    fun test_loaderSize() {
+        val sut = Size
+        assertEquals(80.dp, sut.loaderSize)
+    }
+}
